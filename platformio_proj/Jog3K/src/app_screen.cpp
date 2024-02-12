d2972833d7ccc683cd323da474e6ad7549160442
andrew_marles <andrew@expatria.ca>
2024-02-11 21:09:35 -0800

    Commit with working peripherals.

--- platformio_proj/Jog3K/src/app_screen.cpp
+++ platformio_proj/Jog3K/src/app_screen.cpp
@@ -24,7 +24,6 @@
 #include "hardware/timer.h"
 #include "hardware/gpio.h"
 #include "hardware/flash.h"
-#include <Adafruit_SSD1327.h>
 
 #include "pico/stdio.h"
 #include "pico/time.h"
@@ -33,52 +32,51 @@
 
 #include "i2c_jogger.h"
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+
 #define SPI_TX_PIN 19
 #define SPI_CLK_PIN 18
 #define SPI_CSn_PIN 17
 #define SCR_DC_PIN 16
 #define SCR_RESET_PIN 20
 
+// Color definitions
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
 // Used for software SPI
-#define OLED_CLK SPI_CLK_PIN
-#define OLED_MOSI SPI_TX_PIN
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
 
-// Used for software or hardware SPI
-#define OLED_CS SPI_CSn_PIN
-#define OLED_DC SCR_DC_PIN
+#define SCLK_PIN SPI_CLK_PIN
+#define MOSI_PIN SPI_TX_PIN
+#define DC_PIN   SCR_DC_PIN
+#define CS_PIN   SPI_CSn_PIN
+#define RST_PIN  SCR_RESET_PIN
 
-// Used for I2C or SPI
-#define OLED_RESET -1
+// Color definitions
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
 
-// software SPI
-Adafruit_SSD1327 display(128, 128, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-// hardware SPI
-//Adafruit_SSD1327 display(128, 128, &SPI, OLED_DC, OLED_RESET, OLED_CS);
+const float gxLow  = 0.0;
+const float gxHigh = 100.0;
+const float gyLow  = -512.0;
+const float gyHigh = 512.0;
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+#define LOOP_PERIOD 35 // Display updates every 35 ms
 
 //#define SHOWJOG 1
 //#define SHOWOVER 1
@@ -126,6 +124,207 @@ char buf[8];
 
 const uint8_t * flash_target_contents = (const uint8_t *) (XIP_BASE + FLASH_TARGET_OFFSET);
 
+// Option 1: use any pins but a little slower
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);  
+  // Option 2: must use the hardware SPI pins 
+  // (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
+  // an output. This is much faster - also required if you want
+  // to use the microSD card (see the image drawing example)
+
+Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
+
+float p = 3.1415926;
+
+void testlines(uint16_t color) {
+   tft.fillScreen(BLACK);
+   for (uint16_t x=0; x < tft.width()-1; x+=6) {
+     tft.drawLine(0, 0, x, tft.height()-1, color);
+   }
+   for (uint16_t y=0; y < tft.height()-1; y+=6) {
+     tft.drawLine(0, 0, tft.width()-1, y, color);
+   }
+   
+   tft.fillScreen(BLACK);
+   for (uint16_t x=0; x < tft.width()-1; x+=6) {
+     tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+   }
+   for (uint16_t y=0; y < tft.height()-1; y+=6) {
+     tft.drawLine(tft.width()-1, 0, 0, y, color);
+   }
+   
+   tft.fillScreen(BLACK);
+   for (uint16_t x=0; x < tft.width()-1; x+=6) {
+     tft.drawLine(0, tft.height()-1, x, 0, color);
+   }
+   for (uint16_t y=0; y < tft.height()-1; y+=6) {
+     tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+   }
+
+   tft.fillScreen(BLACK);
+   for (uint16_t x=0; x < tft.width()-1; x+=6) {
+     tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+   }
+   for (uint16_t y=0; y < tft.height()-1; y+=6) {
+     tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+   }
+   
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0,0);
+  tft.setTextColor(color);
+  tft.print(text);
+}
+
+void testfastlines(uint16_t color1, uint16_t color2) {
+   tft.fillScreen(BLACK);
+   for (uint16_t y=0; y < tft.height()-1; y+=5) {
+     tft.drawFastHLine(0, y, tft.width()-1, color1);
+   }
+   for (uint16_t x=0; x < tft.width()-1; x+=5) {
+     tft.drawFastVLine(x, 0, tft.height()-1, color2);
+   }
+}
+
+void testdrawrects(uint16_t color) {
+ tft.fillScreen(BLACK);
+ for (uint16_t x=0; x < tft.height()-1; x+=6) {
+   tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color);
+ }
+}
+
+void testfillrects(uint16_t color1, uint16_t color2) {
+ tft.fillScreen(BLACK);
+ for (uint16_t x=tft.height()-1; x > 6; x-=6) {
+   tft.fillRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color1);
+   tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color2);
+ }
+}
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (uint8_t x=radius; x < tft.width()-1; x+=radius*2) {
+    for (uint8_t y=radius; y < tft.height()-1; y+=radius*2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }  
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (uint8_t x=0; x < tft.width()-1+radius; x+=radius*2) {
+    for (uint8_t y=0; y < tft.height()-1+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }  
+}
+
+void testtriangles() {
+  tft.fillScreen(BLACK);
+  int color = 0xF800;
+  int t;
+  int w = tft.width()/2;
+  int x = tft.height();
+  int y = 0;
+  int z = tft.width();
+  for(t = 0 ; t <= 15; t+=1) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x-=4;
+    y+=4;
+    z-=4;
+    color+=100;
+  }
+}
+
+void testroundrects() {
+  tft.fillScreen(BLACK);
+  int color = 100;
+  
+  int x = 0;
+  int y = 0;
+  int w = tft.width();
+  int h = tft.height();
+  for(int i = 0 ; i <= 24; i++) {
+    tft.drawRoundRect(x, y, w, h, 5, color);
+    x+=2;
+    y+=3;
+    w-=4;
+    h-=6;
+    color+=1100;
+    Serial.println(i);
+  }
+}
+
+void tftPrintTest() {
+  tft.fillScreen(BLACK);
+  tft.setCursor(0, 5);
+  tft.setTextColor(RED);  
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(BLUE);
+  tft.setTextSize(3);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 5);
+  tft.fillScreen(BLACK);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons() {
+ // play
+  tft.fillScreen(BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, GREEN);
+}
+
+/**************************************************************************/
+/*! 
+    @brief  Renders a simple test pattern on the screen
+*/
+/**************************************************************************/
+void lcdTestPattern(void)
+{
+  static const uint16_t PROGMEM colors[] =
+    { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA, BLACK, WHITE };
+
+  for(uint8_t c=0; c<8; c++) {
+    tft.fillRect(0, tft.height() * c / 8, tft.width(), tft.height() / 8,
+      pgm_read_word(&colors[c]));
+  }
+}
+
 void init_multimedia (void){
 
   if (*flash_target_contents != 0xff)
@@ -140,53 +339,94 @@ void init_multimedia (void){
 
   int i;
   for (i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i,pixels.Color(0, 255, 0));
+    pixels.setPixelColor(i,pixels.Color(255, 0, 0));
     pixels.show();   // Send the updated pixel colors to the hardware.
     sleep_ms(50);
   }
 
   sleep_ms(50);
 
-  pixels.setPixelColor(JOGLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(RAISELED,pixels.Color(255, 255, 255)); 
-  pixels.setPixelColor(HALTLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(FEEDLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(SPINLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(COOLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(HOLDLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(RUNLED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(SPINDLELED,pixels.Color(255, 255, 255));
-  pixels.setPixelColor(HOMELED,pixels.Color(255, 255, 255));
 
+   for (i=0; i<NUMPIXELS; i++){
+    pixels.setPixelColor(i,pixels.Color(0, 255, 0));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+  }
+  sleep_ms(50);
   pixels.show();   // Send the updated pixel colors to the hardware.
+  sleep_ms(50);
 
-  display.begin(0x3D);
-  display.clearDisplay();
-  display.display();
+  /*OLED code*/
+  // Option 2: must use the hardware SPI pins 
+  // (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
+  // an output. This is much faster - also required if you want
+  // to use the microSD card (see the image drawing example)
+  SPI.setCS(SPI_CSn_PIN);
+  SPI.setTX(SPI_TX_PIN);
+  SPI.setSCK(SPI_CLK_PIN);
+  // Init Display
+    
 
-  // draw a SSD1327_WHITE circle, 10 pixel radius
-  display.fillCircle(display.width()/2, display.height()/2, 10, SSD1327_WHITE);
-  display.display();
-  delay(1000);
-  display.clearDisplay();
+    tft.begin();
 
-    for (uint8_t rot=0; rot < 4; rot++) {
-    display.setRotation(rot);
-    display.clearDisplay();
-    // text display tests
-    display.setTextSize(1);
-    display.setTextColor(SSD1327_WHITE);
-    display.setCursor(0,0);
-    display.println("Hello, world!");
-    display.setTextColor(SSD1327_BLACK, SSD1327_WHITE); // 'inverted' text
-    display.println(3.141592);
-    display.setTextSize(2);
-    display.setTextColor(SSD1327_WHITE);
-    display.print("0x"); display.println(0xDEADBEEF, HEX);
-    display.display();
+    // You can optionally rotate the display by running the line below.
+    // Note that a value of 0 means no rotation, 1 means 90 clockwise,
+    // 2 means 180 degrees clockwise, and 3 means 270 degrees clockwise.
+    //tft.setRotation(1);
+    // NOTE: The test pattern at the start will NOT be rotated!  The code
+    // for rendering the test pattern talks directly to the display and
+    // ignores any rotation.
+
+    uint16_t time = millis();
+    tft.fillRect(0, 0, 128, 128, BLACK);
+    time = millis() - time;
+            
+    lcdTestPattern();
     delay(100);
     
-  }
+    tft.invert(true);
+    delay(100);
+    tft.invert(false);
+    delay(100);
+
+    tft.fillScreen(BLACK);
+    testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", WHITE);
+    delay(100);
+
+    // tft print function!
+    tftPrintTest();
+    delay(100);
+    
+    //a single pixel
+    tft.drawPixel(tft.width()/2, tft.height()/2, GREEN);
+    delay(100);
+
+    // line draw test
+    testlines(YELLOW);
+    delay(100);    
+  
+    // optimized lines
+    testfastlines(RED, BLUE);
+    delay(100);    
+
+
+    testdrawrects(GREEN);
+    delay(100);
+
+    testfillrects(YELLOW, MAGENTA);
+    delay(100);
+
+    tft.fillScreen(BLACK);
+    testfillcircles(10, BLUE);
+    testdrawcircles(10, WHITE);
+    delay(100);
+    
+    testroundrects();
+    delay(100);
+    
+    testtriangles();
+    delay(100);
+
+    
 
 }
 
